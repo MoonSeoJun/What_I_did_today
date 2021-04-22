cb15e6d7ea35f586be57f0dc6d65ce70fdf22a0c
MoonSeoJun <msj00130@gmail.com>
2021-04-22 16:50:35 +0900

    [Add] Put method create

--- Python/what_to_i_eat/restaurant/__init__.py
+++ Python/what_to_i_eat/restaurant/__init__.py
@@ -1,16 +1,19 @@
 from typing import Mapping
-from flask import Flask, jsonify, request
+from flask import Flask, json, jsonify, request
 from flask_cors import CORS, cross_origin
 import numpy as np
 import pymysql
 
 mydb = pymysql.connect(host="[Hostname]", user="[Username]", 
-		passwd="[Password]", db="[DatabaseName]", charset="utf8")
-cur = mydb.cursor()
+		passwd="[Password]", db="[dbname]", charset="utf8")
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+
+def map_lists(key, value) -> dict:
+    response_value = dict(zip(key, value))
+    return response_value
 
 @app.route('/')
 @cross_origin()
@@ -23,17 +26,30 @@ def index():
     return jsonify(hello="World")
 
 
-@app.route('/datas/<int:id>')
+@app.route('/datas/<int:id>', methods=('GET','PUT'))
 @cross_origin()
 def datas(id):
-    user_info = ['id','name','cost']
+    cur = mydb.cursor()
+    if request.method == 'PUT':
+        name = request.json["name"]
+        cost = request.json["cost"]
+        try:
+            cur.execute(f"update `restaurant`.`users` set name='{name}', cost={cost} where id={id};")
+            mydb.commit()
+            cur.close()
+            return jsonify(True)
+        except:
+            return jsonify(False)
+
     cur.execute(f"select * from `restaurant`.`users` where id={id};")
+    user_info = ['id','name','cost']
     data_list = list(cur.fetchone())
-    response_json = dict(zip(user_info, data_list))
+    response_json = map_lists(user_info, data_list)
+    cur.close()
     return jsonify(user=response_json)
 
 
-@app.route('/post-test', methods=['POST', 'GET'])
+@app.route('/post-test', methods=('POST', 'GET'))
 def test_post():
     if request.method == 'POST':
         id = request.json["id"]

bc9e0f4da692d843654ba16040ebb1bc8c72c0de
MoonSeoJun <msj00130@gmail.com>
2021-04-22 20:27:28 +0900

    [Add] Create post method for insert user

--- Python/what_to_i_eat/restaurant/__init__.py
+++ Python/what_to_i_eat/restaurant/__init__.py
@@ -1,11 +1,10 @@
 from typing import Mapping
 from flask import Flask, json, jsonify, request
 from flask_cors import CORS, cross_origin
-import numpy as np
 import pymysql
 
-mydb = pymysql.connect(host="[Hostname]", user="[Username]", 
-		passwd="[Password]", db="[dbname]", charset="utf8")
+mydb = pymysql.connect(host="[HOSTNAME]", user="[USERNAME]", 
+		passwd="[PASSWORD]", db="[DBNAME]", charset="utf8")
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -15,10 +14,12 @@ def map_lists(key, value) -> dict:
     response_value = dict(zip(key, value))
     return response_value
 
+
 @app.route('/')
 @cross_origin()
 def main():
-    return jsonify(main={"temp":24,"feels_like":23.53,"temp_min":24,"temp_max":24,"pressure":1020,"humidity":41})
+    return jsonify(message="Here is Main Page")
+
 
 @app.route('/hello')
 @cross_origin()
@@ -31,9 +32,9 @@ def index():
 def datas(id):
     cur = mydb.cursor()
     if request.method == 'PUT':
-        name = request.json["name"]
-        cost = request.json["cost"]
         try:
+            name = request.json["name"]
+            cost = request.json["cost"]
             cur.execute(f"update `restaurant`.`users` set name='{name}', cost={cost} where id={id};")
             mydb.commit()
             cur.close()
@@ -49,9 +50,27 @@ def datas(id):
     return jsonify(user=response_json)
 
 
+@app.route('/datas', methods=('GET', 'POST'))
+@cross_origin()
+def data():
+    cur = mydb.cursor()
+    if request.method == 'POST':
+        try:
+            name = request.json["name"]
+            cost = request.json["cost"]
+            cur.execute(f"insert  into `restaurant`.`users` (name, cost) values ('{name}',{cost})")
+            mydb.commit()
+            cur.close()
+            return jsonify(True)
+        except:
+            return jsonify(False)
+
+
 @app.route('/post-test', methods=('POST', 'GET'))
+@cross_origin()
 def test_post():
     if request.method == 'POST':
         id = request.json["id"]
-        return jsonify(id=id)
+        return jsonify(Id=id)
     return jsonify(hello="World")
+
--- Python/what_to_i_eat/test.py
+++ Python/what_to_i_eat/test.py
@@ -1,5 +1,4 @@
 import pymysql
-import numpy as np
 
 db = pymysql.connect(host="localhost", user="root", passwd="m-s-j-0-1-3-0", db="restaurant", charset="utf8")
 cur = db.cursor()
@@ -8,7 +7,7 @@ id = 1
 
 user_info = ['id','name','cost']
 cur.execute(f"select * from `restaurant`.`users` where id={id};")
-data_list = np.asarray(cur.fetchone())
+data_list = list(cur.fetchone())
 print(data_list)
 response_json = dict(zip(user_info, data_list))
 
